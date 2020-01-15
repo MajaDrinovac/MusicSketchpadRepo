dbed66799d42b1cb8e5436fece141a59ecea2227
alinakhan02 <alina02.khan@gmx.at>
2020-01-15 18:22:59 +0100

    Grid (Responsive)

--- programm/src/app/app.module.ts
+++ programm/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
     BrowserModule,
     AppRoutingModule,
     MatSidenavModule,
+    MatGridListModule,
     BrowserAnimationsModule
   ],
   providers: [],
