baa0e8d461dfbeebca96aaf529711d7938c92aec
maja26 <43316150+maja26@users.noreply.github.com>
2020-01-17 21:58:38 +0100

    sketchpad canvas added

--- programm/src/app/sketchpad/sketchpad.component.ts
+++ programm/src/app/sketchpad/sketchpad.component.ts
@@ -12,18 +12,32 @@ declare let ml5:any
 })
 export class SketchpadComponent implements OnInit {
   private drawp5:p5
+  private editp5:p5
+  private model
 
   constructor() { 
-
+    let options = {
+      inputs: ['x', 'y'],
+      output: ['label'],
+      task: 'classification'
+    }
+    this.model = ml5.neuralNetwork(options)
   }
 
   ngOnInit() {
     this.drawp5 = new p5(this.sketch)
+    this.editp5 = new p5(this.editSketch)
   }
 
   private sketch = (s) =>{
     s.setup = () =>{
-      let canv = s.createCanvas(500, 500).parent(document.getElementById("canv"))
+      let canv = s.createCanvas(document.getElementsByClassName("content")[0].clientWidth/2, document.getElementsByClassName("content")[0].clientHeight*2/3).parent(document.getElementById("canv"))
+      s.background(208, 208, 208)
+    }
+  }
+  private editSketch = (s) =>{
+    s.setup = () =>{
+      let canv = s.createCanvas(document.getElementsByClassName("content")[0].clientWidth/2, document.getElementsByClassName("content")[0].clientHeight*2/3).parent(document.getElementById("canvEditMode"))
       s.background(208, 208, 208)
     }
   }
