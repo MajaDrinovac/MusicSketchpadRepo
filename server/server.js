ed4a6d423542cdf1ddb23745670d03e28f765e36
maja26 <43316150+maja26@users.noreply.github.com>
2020-03-23 20:51:14 +0100

    added methods

--- server/server.js
+++ server/server.js
@@ -9,18 +9,40 @@ app.use(bodyParser.json())
 
 //message - testen ob der serer funktioniert
 app.get('/message', function(req,res){
-    res.send('Hello World');
+    res.send('Server works!');
+})
+
+app.get("/findAllUsers", function(req, res){
+    let users = [
+        {"username": "Maja", "email": "maja.drino@gmail.com", "password":"secret"}
+    ]
+
+    //TODO: Find users in DB
+
+    res.send(users)
 })
 
 
-//post req
-app.post('/p', function (req, res) {
+//register
+app.post('/createUser', function (req, res) {
     console.log(req.body)
-    res.send('Got a POST request');
+    let user = req.body
+
+    //TODO: create new user in DB 
+
+    res.send('Got the user');
   });
-  
+
+//login
+app.post('/login', function(req, res){
+    let username = req.body.username
+    
+    //TODO: Find in DB
+
+    res.send("User "+ req.body.username +" exists")
+})
 
 //Server is listening on port 3000
 app.listen(3000, function (){
-    console.log('Server listening on port 3000!')
+    console.log('Server listening on port 3000')
 })
