b1de2ff539cd47e52dfc4412ab3a5c49d3b643d5
ninamayer01 <ninamayer0815@gmail.com>
2020-03-29 00:06:13 +0100

    funktion erstellt

--- programm/src/app/login/login.component.ts
+++ programm/src/app/login/login.component.ts
@@ -18,4 +18,20 @@ export class LoginComponent implements OnInit {
     Validators.email,
   ]);
 
+  createJSON() {
+    const email: String = document.getElementById("emailAdresse").value;
+    const passwort: String = document.getElementById("passwort").value;
+
+    if(email != "" && email == "") {
+      alert(email);
+    }
+
+    alert(passwort);
+  }
+
+  validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
 }
