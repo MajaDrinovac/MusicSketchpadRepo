13a52c6ad3a678e3fd91ce45e9d067b61a00677c
maja26 <43316150+maja26@users.noreply.github.com>
2020-04-27 19:49:32 +0200

    von bibliothek zu sketchpad seite

--- programm/src/app/bibliothek/bibliothek.component.ts
+++ programm/src/app/bibliothek/bibliothek.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
 import { SoundFontPlayer, INoteSequence } from '@magenta/music/es6';
 import * as mm from '@magenta/music/es6'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bibliothek',
@@ -12,7 +13,7 @@ export class BibliothekComponent implements OnInit {
   public melodies
   private sequence:INoteSequence
   private soundfont_player:SoundFontPlayer
-  constructor(private httpService:HttpService) { 
+  constructor(public router:Router,private httpService:HttpService) { 
     this.soundfont_player = new mm.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
   }
 
@@ -35,5 +36,10 @@ export class BibliothekComponent implements OnInit {
     for(let i = 0; i < melody.melody.length; i++){
       players[i].start(melody.melody[i])
     }
+  }
+
+  openEditMode(melody){
+    
+    this.router.navigate(['/sketchpad'])
   }
 }
